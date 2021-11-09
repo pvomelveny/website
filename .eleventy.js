e05a2fe2c817496313f5a4f71d3d5d15c94d777b
Patrick O'Melveny <pvomelveny@gmail.com>
2021-11-08 18:31:30 -0800

    Following a tutorial for a marketing site. We'll refine from here

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css");
 
     return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+
         dir: {
             input: "src",
             output: "public",
+
+            layouts: '_includes/layouts'
         },
     };
 };
